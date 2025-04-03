39b1e8432751287fc11fc0bab16d521894472f80
Jacob Cable <jacobcable94@gmail.com>
2025-04-03 14:40:59 +0100

    chore: update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// next.config.js
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'us-central1-dev-extensions-testing.cloudfunctions.net',
+        pathname: '/**',
+      },
+    ],
+  },
+};
